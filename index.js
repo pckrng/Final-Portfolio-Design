c3debed9c75cc7fe9916a518aa94e0a7953afa00
pckrng <pckrng@outlook.com>
2021-01-29 14:29:51 +0000

    darkmode button move

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
     var options = {
-        bottom: '710px', // default: '32px'
-        right: '32px', // default: '32px'
+        bottom: '30px', // default: '32px'
+        left: '32px', // default: '32px'
         left: 'unset', // default: 'unset'
         time: '1s', // default: '0.3s'
         mixColor: '#fff', // default: '#fff'
         backgroundColor: '#e2e2e2',  // default: '#fff'
-        buttonColorDark: '#100f2c',  // default: '#100f2c'
-        buttonColorLight: '#fff', // default: '#fff'
+        buttonColorDark: '#8C8C8C',  // default: '#100f2c'
+        buttonColorLight: 'rgb(223, 220, 84)', // default: '#fff'
         saveInCookies: true, // default: true,
         label: '💡' // default: ''
     }
@@ -14,7 +14,8 @@
     const darkmode = new Darkmode(options);
     darkmode.showWidget();
 
-    gsap.from(".flyIn", {duration: 2, x: -300, opacity: 0, scale: 0.1, ease: "bounce.out"});
+    gsap.from(".flyIn", {duration: 2, x: -300, opacity: 0, scale: 0.1, ease: "bounce.out"}); 
+    gsap.from(".container", {duration: 2, x: -300, opacity: 0, scale: 0.1, ease: "bounce.out"}); 
     gsap.from("a", {duration: 3, opacity: 0});
 
     new fullpage('#fullpage', {
